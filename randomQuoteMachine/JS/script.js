100a115dfb4363a4215a377c5f5a30b3cb27655a
forgoroe <forgoroe@gmail.com>
2016-10-09 15:34:55 +0200

    add progress

--- randomQuoteMachine/JS/script.js
+++ randomQuoteMachine/JS/script.js
@@ -1,18 +1,38 @@
+var quoteText = "Meet the dusk";
+var quoteAuthor = "Sirius, Battlerite";
+var quotes = new Array();
+var counter = 0;
+
 window.onload = function() {
-    if (window.jQuery) {
-        // jQuery is loaded
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
+  requestQuote();
+  writeQuote();
 
-    document.getElementById("quote").innerHTML = "Meet the dusk - Sirius, Battlerite";
-
-    $('button').click(function(){
-      var forismaticAPI = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
-      $.getJSON(forismaticAPI, function(data) {
-      document.getElementById("quote").innerHTML = data.quoteText + ' - ' + data.quoteAuthor;
-    });
+$('button').click(function getQuote(){
+  writeQuote();
+  requestQuote();
   });
+};
+
+function requestQuote(){
+  var forismaticAPI = "http://api.forismatic.com/api/1.0/?method=getQuote";
+  var format = "&format=json";
+  var lang = "&lang=en";
+  var fullRequest = forismaticAPI+format+lang;
+
+  $.getJSON(fullRequest, function(data) {
+    quotes.push({
+      quoteText: data.quoteText,
+      quoteAuthor: data.quoteAuthor
+    });
+//  console.log(quotes);
+});
+}
+
+function writeQuote(){
+  console.log(quotes[counter]);
+
+
+//  document.getElementById("quoteText").innerHTML = quotes[counter].quoteText;
+//  document.getElementById("quoteAuthor").innerHTML = quotes[counter].quoteAuthor;
+//  counter++;
 }
