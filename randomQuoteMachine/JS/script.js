3c4fab3b0523672020b4115af85f5fcf1c804896
forgoroe <forgoroe@gmail.com>
2016-10-10 12:50:40 +0200

    add bootstrap, style elements, add tweet out button

--- randomQuoteMachine/JS/script.js
+++ randomQuoteMachine/JS/script.js
@@ -3,34 +3,38 @@ var quoteAuthor;
 var quote = {};
 
 window.onload = function() {
-var relaxTime = 3.5;
-$('button').click(function (){
-    requestQuote();
-    var button = $(this);
-    button.prop('disabled', true);
-    setTimeout(function(){
-      button.prop('disabled', false);
-    }, relaxTime*1000);
-  });
+    var relaxTime = 3.5;
+    $('#nextQuoteBtn').click(function() {
+        requestQuote();
+        var button = $(this);
+        button.prop('value', relaxTime);
+        button.prop('disabled', true);
 
-function requestQuote(){
-  var forismaticAPI = "http://api.forismatic.com/api/1.0/?method=getQuote";
-  var format = "&format=json";
-  var lang = "&lang=en";
-  var fullRequest = forismaticAPI+format+lang;
+        setTimeout(function() {
+            button.prop('disabled', false);
+        }, relaxTime * 1000);
+    });
 
-  $.getJSON(fullRequest, function(data) {
-    quote = {
-      quoteText: data.quoteText,
-      quoteAuthor: data.quoteAuthor
-    };
-    console.log(quote);
-    displayQuote(quote);
-});
-}
 
-function displayQuote(){
-    document.getElementById("quoteText").innerHTML = quote.quoteText;
-    document.getElementById("quoteAuthor").innerHTML = quote.quoteAuthor;
-  }
+
+    function requestQuote() {
+        var forismaticAPI = "http://api.forismatic.com/api/1.0/?method=getQuote";
+        var format = "&format=json";
+        var lang = "&lang=en";
+        var fullRequest = forismaticAPI + format + lang;
+
+        $.getJSON(fullRequest, function(data) {
+            quote = {
+                quoteText: data.quoteText,
+                quoteAuthor: data.quoteAuthor
+            };
+            console.log(quote);
+            displayQuote(quote);
+        });
+    }
+
+    function displayQuote() {
+        document.getElementById("quoteText").innerHTML = quote.quoteText;
+        document.getElementById("quoteAuthor").innerHTML = quote.quoteAuthor;
+    }
 };
